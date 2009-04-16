7e9332b2ca63cc514b272ebb5ca2b3dd84d09b5d
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2009-04-16 15:33:47 +0200

    Remove unnecessary @Before from EndpointTest

--- src/nl/rug/escher/entities/test/EndpointTest.java
+++ src/nl/rug/escher/entities/test/EndpointTest.java
@@ -2,26 +2,19 @@ package nl.rug.escher.entities.test;
 
 import nl.rug.escher.entities.Endpoint;
 
-import org.junit.Before;
 import org.junit.Test;
 
 
 public class EndpointTest { 
-	private Endpoint d_endpoint;
-
-	@Before
-	public void setUp() throws Exception {
-		d_endpoint = new Endpoint();
-	}
 	
 	@Test
 	public void testSetDescription() {
-		Helper.testSetter(d_endpoint, Endpoint.PROPERTY_DESCRIPTION, null, "My Description");
+		Helper.testSetter(new Endpoint(), Endpoint.PROPERTY_DESCRIPTION, null, "My Description");
 	}
 
 	@Test
 	public void testSetName() {
-		Helper.testSetter(d_endpoint, Endpoint.PROPERTY_NAME, null, "My Name");
+		Helper.testSetter(new Endpoint(), Endpoint.PROPERTY_NAME, null, "My Name");
 	}
 
 }
