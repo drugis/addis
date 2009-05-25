56f2a90bed12b31fc865dd71cb9aa2f95b720b27
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2009-05-25 09:34:34 +0200

    Test class

---
+++ src/nl/rug/escher/addis/entities/test/DomainPersistentTest.java
@@ -0,0 +1,51 @@
+package nl.rug.escher.addis.entities.test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import nl.rug.escher.addis.entities.Domain;
+import nl.rug.escher.addis.entities.DomainPersistent;
+import nl.rug.escher.addis.entities.Endpoint;
+import nl.rug.escher.addis.entities.Study;
+
+public class DomainPersistentTest {
+	@Before
+	public void setUp() {
+		File f = new File("test.db");
+		if (f.exists() && !f.delete()) {
+			throw new RuntimeException();
+		}
+	}
+	
+	private PersistenceManagerFactory getFactory() {
+		return JDOHelper.getPersistenceManagerFactory("datanucleus.test.properties");
+	}
+	
+	@Test
+	public void testPersistEndpoint() {
+		Domain domain = new DomainPersistent(getFactory());
+		
+		Endpoint e = new Endpoint("e");
+		domain.addEndpoint(e);
+		
+		assertEquals(e, domain.getEndpoint("e"));
+	}
+	
+	@Test
+	public void testPersistStudyWithEndpoint() {
+		Domain domain = new DomainPersistent(getFactory());
+		domain.addEndpoint(new Endpoint("e"));
+		
+		Study s = new Study();
+		s.setId("STUDY");
+		s.setEndpoints(new ArrayList<Endpoint>());
+	}
+}
