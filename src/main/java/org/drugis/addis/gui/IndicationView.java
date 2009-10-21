e8cd273900922b55fe1530da356b03ac867c22e4
Tijs Zwinkels <github@tumblecow.net>
2009-10-21 15:59:11 +0200

    Started Implementation of IndicationView.

--- src/main/java/org/drugis/addis/gui/IndicationView.java
+++ src/main/java/org/drugis/addis/gui/IndicationView.java
@@ -9,6 +9,10 @@ import org.drugis.addis.presentation.PresentationModelManager;
 import org.drugis.common.gui.ViewBuilder;
 
 import com.jgoodies.binding.PresentationModel;
+import com.jgoodies.binding.adapter.BasicComponentFactory;
+import com.jgoodies.forms.builder.PanelBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
 
 public class IndicationView implements ViewBuilder {
 	
@@ -24,7 +28,29 @@ public class IndicationView implements ViewBuilder {
 	}
 	
 	public JComponent buildPanel() {
-		return new JLabel("Indications");
+		FormLayout layout = new FormLayout(
+				"right:pref, 3dlu, pref",
+				"p, 3dlu, p, 3dlu, p, 3dlu, p, 3dlu, p");
+		
+		PanelBuilder builder = new PanelBuilder(layout);
+		builder.setDefaultDialogBorder();
+		
+		CellConstraints cc =  new CellConstraints();
+		
+		builder.addSeparator("Indication", cc.xyw(1, 1, 3));
+		builder.addLabel("Concept ID:", cc.xy(1, 3));
+		
+//		builder.add(BasicComponentFactory.createLabel(
+//				d_pm.getModel(Indication.PROPERTY_CODE)), cc.xy(3, 3));
+		
+		
+		builder.addLabel("Fully Specified Name:", cc.xy(1, 5));
+		builder.add(BasicComponentFactory.createLabel(
+				d_pm.getModel(Indication.PROPERTY_NAME)), cc.xy(3, 5));
+		
+		builder.addSeparator("Studies", cc.xyw(1, 7, 3));
+		
+		return builder.getPanel();
 	}
 	
 
