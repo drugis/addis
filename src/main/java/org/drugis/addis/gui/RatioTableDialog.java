ea3e585f7cd421f74823ec7201be28162e5fd9b5
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2009-11-02 13:52:35 +0100

    Added celrenderers for RatioTable.

--- src/main/java/org/drugis/addis/gui/RatioTableDialog.java
+++ src/main/java/org/drugis/addis/gui/RatioTableDialog.java
@@ -1,6 +1,7 @@
 package org.drugis.addis.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
@@ -10,7 +11,12 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableModel;
+
+import org.drugis.addis.presentation.LabeledPresentationModel;
+
+import com.jgoodies.binding.adapter.BasicComponentFactory;
 
 @SuppressWarnings("serial")
 public class RatioTableDialog extends JDialog {
@@ -24,8 +30,16 @@ public class RatioTableDialog extends JDialog {
 		pack();
 	}
 	
+	private class RatioTableCellRenderer implements TableCellRenderer {
+		public Component getTableCellRendererComponent(JTable table,
+				Object val, boolean isSelected, boolean hasFocus, int row, int col) {
+			return BasicComponentFactory.createLabel(((LabeledPresentationModel)val).getLabelModel());
+		}
+	}
+	
 	private void initComps() {
 		JTable table = new JTable(d_tableModel);
+		table.setDefaultRenderer(Object.class, new RatioTableCellRenderer());
 		
 		JPanel panel = new JPanel(new BorderLayout());
 		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
