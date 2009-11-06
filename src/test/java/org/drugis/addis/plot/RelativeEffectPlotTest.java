fa7da8567aae1ec51433111ea25c3319c14abd91
Maarten Jacobs <maartenjisokee@home.nl>
2009-11-06 14:04:18 +0100

    Fix

--- src/test/java/org/drugis/addis/plot/RelativeEffectPlotTest.java
+++ src/test/java/org/drugis/addis/plot/RelativeEffectPlotTest.java
@@ -10,6 +10,7 @@ public class RelativeEffectPlotTest {
 	@Test
 	public void testNormalPlot() {
 		MockGraphics2D g2d = new MockGraphics2D(Collections.singleton(new Line(0, 11, 100, 11)));
+		// (Also add the mean-box)
 		// Make some BinnedScale that maps [0, 1] -> [0, 200]
 		// RelativeEffect with Interval [0, 0.5]
 		// RelativeEffectPlot plot = ...
