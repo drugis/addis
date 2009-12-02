2d7929bcf11be00b7e684dadb8fca0e77cd02be4
Tijs Zwinkels <github@tumblecow.net>
2009-12-02 17:27:01 +0100

    some extra tests

--- src/test/java/org/drugis/addis/plot/LineTest.java
+++ src/test/java/org/drugis/addis/plot/LineTest.java
@@ -18,13 +18,19 @@ public class LineTest {
 	public void testDiscriminateP1() {
 		Line unexpected = new Line(15, 20, 30, 40);
 		Line2D actual = new Line2D.Double(10, 20, 30, 40);
-		assertNotSame(unexpected, actual);
+		assertTrue(!unexpected.equals(actual));
 	}
 	
 	@Test
 	public void testDiscriminateP2() {
 		Line unexpected = new Line(10, 20, 30, 45);
 		Line2D actual = new Line2D.Double(10, 20, 30, 40);
-		assertNotSame(unexpected, actual);
+		assertTrue(!unexpected.equals(actual));
+	}
+	
+	@Test
+	public void testToString() {
+		Line l = new Line(10,20,30,40);
+		assertEquals("Line(x1 = 10.0, y1 = 20.0, x2 = 30.0, y2 = 40.0)", l.toString());
 	}
 }
--- src/test/java/org/drugis/addis/presentation/RandomEffectsMetaAnalysisPresentationTest.java
+++ src/test/java/org/drugis/addis/presentation/RandomEffectsMetaAnalysisPresentationTest.java
@@ -2,13 +2,18 @@ package org.drugis.addis.presentation;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import org.drugis.addis.ExampleData;
 import org.drugis.addis.entities.Domain;
 import org.drugis.addis.entities.DomainImpl;
 import org.drugis.addis.entities.Endpoint;
+import org.drugis.addis.entities.MeanDifference;
+import org.drugis.addis.entities.Study;
 import org.drugis.addis.entities.metaanalysis.RandomEffectsMetaAnalysis;
+import org.drugis.common.JUnitUtil;
 import org.junit.Test;
 
 public class RandomEffectsMetaAnalysisPresentationTest {
@@ -39,4 +44,41 @@ public class RandomEffectsMetaAnalysisPresentationTest {
 		RandomEffectsMetaAnalysisPresentation pres = (RandomEffectsMetaAnalysisPresentation) fact.getModel(meta);
 		assertEquals(Endpoint.Type.CONTINUOUS, pres.getAnalysisType());
 	}
+	
+	@Test
+	public void testGetIncludedStudies() {
+		RandomEffectsMetaAnalysis meta = new RandomEffectsMetaAnalysis("meta",
+				ExampleData.buildEndpointCgi(),
+				Collections.singletonList(ExampleData.buildStudyChouinard()),
+				ExampleData.buildDrugFluoxetine(),
+				ExampleData.buildDrugParoxetine());
+		Domain domain = new DomainImpl();
+		ExampleData.initDefaultData(domain );
+		PresentationModelFactory fact = new PresentationModelFactory(domain);
+		RandomEffectsMetaAnalysisPresentation pres = (RandomEffectsMetaAnalysisPresentation) fact.getModel(meta);
+		List<Study> expected = new ArrayList<Study>();
+		expected.add(ExampleData.buildStudyChouinard());
+
+		JUnitUtil.assertAllAndOnly(expected, pres.getIncludedStudies().getValue());
+	}
+	
+	@Test
+	public void testGetForestPlotPresentation() {
+		RandomEffectsMetaAnalysis meta = new RandomEffectsMetaAnalysis("meta",
+				ExampleData.buildEndpointCgi(),
+				Collections.singletonList(ExampleData.buildStudyChouinard()),
+				ExampleData.buildDrugFluoxetine(),
+				ExampleData.buildDrugParoxetine());
+		Domain domain = new DomainImpl();
+		ExampleData.initDefaultData(domain );
+		PresentationModelFactory fact = new PresentationModelFactory(domain);
+		RandomEffectsMetaAnalysisPresentation pres = (RandomEffectsMetaAnalysisPresentation) fact.getModel(meta);
+		
+		ForestPlotPresentation expected = new ForestPlotPresentation(meta, MeanDifference.class);
+		ForestPlotPresentation actual = pres.getForestPlotPresentation(MeanDifference.class);
+		assertEquals(expected.getRelativeEffectAt(0).getRelativeEffect(), actual.getRelativeEffectAt(0).getRelativeEffect(), 0.001);
+		assertEquals(expected.getHeterogeneity(), actual.getHeterogeneity());
+		assertEquals(expected.getHeterogeneityI2(), actual.getHeterogeneityI2());
+	}
+	
 }
