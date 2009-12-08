88cd05f041b0faf3a94b9ae7ab14a686458fa6f7
Tijs Zwinkels <github@tumblecow.net>
2009-12-08 14:29:45 +0100

    Test that build breaks on 1.6 code.

--- src/main/java/org/drugis/addis/gui/components/HtmlWordWrapper.java
+++ src/main/java/org/drugis/addis/gui/components/HtmlWordWrapper.java
@@ -11,7 +11,8 @@ public class HtmlWordWrapper {
 		String[] arr = makeParts(input);
 		String resStr = "";
 		for (String s : arr) {
-			if (s.isEmpty()) {
+			s.isEmpty();
+			if (s.length() < 1) {
 				continue;
 			}
 			if(!resStr.equals("")) {
