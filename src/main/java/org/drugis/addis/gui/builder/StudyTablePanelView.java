dd056f9914d7b501c814431d30b6d1b87bcd1e7b
Hanno Koeslag <hanno@planet.nl>
2010-02-10 16:37:15 +0100

    increased size of studies table

--- src/main/java/org/drugis/addis/gui/builder/StudyTablePanelView.java
+++ src/main/java/org/drugis/addis/gui/builder/StudyTablePanelView.java
@@ -59,7 +59,7 @@ public class StudyTablePanelView implements ViewBuilder {
 		table.addKeyListener(new EntityTableDeleteListener((Main) d_parent));
 		
 		JScrollPane pane = new JScrollPane(table);
-		pane.setPreferredSize(new Dimension(200, 250));
+		pane.setPreferredSize(new Dimension(200, 450));
 		pane.setBorder(BorderFactory.createEmptyBorder());
 		pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
