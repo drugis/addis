8364c8a88d6859fe220fee2e094e2922d465e07d
Tijs Zwinkels <github@tumblecow.net>
2010-06-18 17:16:05 +0200

    Release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION=$1
+DIR=addis-$VERSION
+DATA=$DIR/data
+
+mkdir -p $DATA
+
+cp target/addis-$VERSION-jar-with-dependencies.jar $DIR/addis-$VERSION.jar
+cp LICENSE.txt $DIR
+cp README.txt $DIR
+cp hansen.xml $DATA
+cp hansen-analyses.xml $DATA 
+
+zip -r addis-$VERSION.zip $DIR
