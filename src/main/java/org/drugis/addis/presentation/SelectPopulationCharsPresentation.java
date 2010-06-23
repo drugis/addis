695f4e6fc2be9eacd0891aa243fb5f34e5932622
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-06-23 16:42:30 +0200

    Fix: enable adding popchars from AddStudyWizard even if none exist in domain

--- src/main/java/org/drugis/addis/presentation/SelectPopulationCharsPresentation.java
+++ src/main/java/org/drugis/addis/presentation/SelectPopulationCharsPresentation.java
@@ -21,14 +21,8 @@
 
 package org.drugis.addis.presentation;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.util.List;
-
 import org.drugis.addis.entities.PopulationCharacteristic;
 import org.drugis.addis.gui.Main;
-
-import com.jgoodies.binding.value.AbstractValueModel;
 
 @SuppressWarnings("serial")
 public class SelectPopulationCharsPresentation
@@ -36,47 +30,10 @@ extends SelectFromFiniteListPresentationImpl<PopulationCharacteristic> {
 	public SelectPopulationCharsPresentation(ListHolder<PopulationCharacteristic> options, Main main) {
 		super(options, "Population Baseline Characteristics", "Select Population Baseline Characteristics",
 			"Please select the appropriate population baseline characteristics.", main);
-		d_addSlotsEnabled = new AddSlotsEnabledModel();
 	}
 	
 	@Override
 	public void showAddOptionDialog(int idx) {
 		d_main.showAddPopulationCharacteristicDialog(getSlot(idx));
-	}
-	
-	public class AddSlotsEnabledModel extends AbstractValueModel implements PropertyChangeListener {
-		public AddSlotsEnabledModel() {
-			d_options.addValueChangeListener(this);
-			SelectPopulationCharsPresentation.this.addPropertyChangeListener(this);
-		}
-		
-		public Object getValue() {
-			return addSlotsEnabled();
-		}
-
-		private boolean addSlotsEnabled() {
-			return addSlotsEnabled(d_slots.size(), d_options.getValue().size());
-		}
-		
-		private boolean addSlotsEnabled(int slots, int options) {
-			return slots < options;
-		}
-
-		public void setValue(Object newValue) {
-			throw new RuntimeException("AddSlotsEnabledModel is read-only");
-		}
-
-		@SuppressWarnings("unchecked")
-		public void propertyChange(PropertyChangeEvent evt) {
-			if (evt.getSource() == d_options) {
-				boolean oldVal = addSlotsEnabled(d_slots.size(), ((List)evt.getOldValue()).size());
-				boolean newVal = addSlotsEnabled(d_slots.size(), ((List)evt.getNewValue()).size());
-				fireValueChange(oldVal, newVal);
-			} else if (evt.getSource() == SelectPopulationCharsPresentation.this) {
-				boolean oldVal = addSlotsEnabled((Integer)evt.getOldValue(), d_options.getValue().size());
-				boolean newVal = addSlotsEnabled((Integer)evt.getNewValue(), d_options.getValue().size());
-				fireValueChange(oldVal, newVal);
-			}
-		}
 	}
 }
