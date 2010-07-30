847c81fc36c2b2c2d07e47eb4b2295bc1052e5d5
Tijs Zwinkels <github@tumblecow.net>
2010-07-30 10:36:13 +0200

    changed network meta analysis rank probability plot to be consistent with BRA

--- src/test/java/org/drugis/addis/util/threading/ThreadHandlerTest.java
+++ src/test/java/org/drugis/addis/util/threading/ThreadHandlerTest.java
@@ -1,6 +1,6 @@
 package org.drugis.addis.util.threading;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
