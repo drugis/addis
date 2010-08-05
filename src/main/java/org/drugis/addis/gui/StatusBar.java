370e396e73fdcd3e539456800fcfbc94b2e538ec
Tijs Zwinkels <github@tumblecow.net>
2010-08-05 15:32:41 +0200

    display threads in queue/running in the status bar

--- src/main/java/org/drugis/addis/gui/StatusBar.java
+++ src/main/java/org/drugis/addis/gui/StatusBar.java
@@ -1,13 +1,27 @@
 package org.drugis.addis.gui;
 
+import java.util.Formatter;
+
 import javax.swing.JLabel;
 import javax.swing.JToolBar;
 
+import org.drugis.addis.presentation.PresentationModelFactory;
+import org.drugis.addis.presentation.ThreadHandlerPresentation;
+import org.drugis.common.gui.OneWayObjectFormat;
+
+import com.jgoodies.binding.adapter.BasicComponentFactory;
+
+import sun.util.calendar.BaseCalendar;
+
 public class StatusBar extends JToolBar {
+	private ThreadHandlerPresentation d_ThreadHandlerPresentation = new ThreadHandlerPresentation();
 	
 	public StatusBar(){
 		super();
-		add(new JLabel("Test Button"));
+		add(new JLabel("  Running threads:"));
+		add(BasicComponentFactory.createLabel(d_ThreadHandlerPresentation.getRunningThreads(), new OneWayObjectFormat()));
+		add(new JLabel("    Threads in queue:"));
+		add(BasicComponentFactory.createLabel(d_ThreadHandlerPresentation.getThreadsInQueue(), new OneWayObjectFormat()));
 	}
 	
 
