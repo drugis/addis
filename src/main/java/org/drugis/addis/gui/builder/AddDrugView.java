37575919652cb6dcab2a977403fb8c8102c0ccdc
Florin Schimbinschi <florinsch@gmail.com>
2010-08-20 11:49:38 +0200

    Added thread support for searching atc codes. Clicking on the button displays a loading image on the button now. (Fixed #17).

--- src/main/java/org/drugis/addis/gui/builder/AddDrugView.java
+++ src/main/java/org/drugis/addis/gui/builder/AddDrugView.java
@@ -22,6 +22,7 @@
 package org.drugis.addis.gui.builder;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
@@ -35,6 +36,8 @@ import org.drugis.addis.entities.Drug;
 import org.drugis.addis.gui.GUIFactory;
 import org.drugis.addis.gui.components.NotEmptyValidator;
 import org.drugis.addis.util.AtcParser;
+import org.drugis.addis.util.RunnableReadyModel;
+import org.drugis.common.ImageLoader;
 import org.drugis.common.gui.ViewBuilder;
 
 import com.jgoodies.binding.PresentationModel;
@@ -45,7 +48,8 @@ import com.jgoodies.forms.layout.FormLayout;
 
 public class AddDrugView implements ViewBuilder {
 	private JTextField d_name;
-	private JTextField d_atcCode;
+	private JTextField d_atcCodeTextField;
+	private String d_atcCode;
 	private JButton d_loadButton;
 	private PresentationModel<Drug> d_model;
 	private NotEmptyValidator d_validator; 
@@ -57,30 +61,51 @@ public class AddDrugView implements ViewBuilder {
 	
 	@SuppressWarnings("serial")
 	public void initComponents() {
+		d_atcCode = null;
 		d_name = BasicComponentFactory.createTextField(d_model.getModel(Drug.PROPERTY_NAME), false);
 		d_name.setColumns(15);
 		d_validator.add(d_name);
-		d_loadButton = GUIFactory.createIconButton(FileNames.ICON_SEARCH,
-		"Search ATC Code");
-		d_atcCode = BasicComponentFactory.createTextField(d_model.getModel(Drug.PROPERTY_ATCCODE), false);
-		d_validator.add(d_atcCode);
+		d_loadButton = GUIFactory.createIconButton(FileNames.ICON_SEARCH, "Search ATC Code");
+		d_atcCodeTextField = BasicComponentFactory.createTextField(d_model.getModel(Drug.PROPERTY_ATCCODE), false);
+		d_validator.add(d_atcCodeTextField);
 		
 		d_loadButton.addActionListener(new AbstractAction() {
 			public void actionPerformed(ActionEvent arg0) {
-				try {					
-					String code = new AtcParser().getAtcCode(d_model.getModel(Drug.PROPERTY_NAME).getString()).getCode();
-					d_model.getModel(Drug.PROPERTY_ATCCODE).setValue(code);
-					if(code == null) {
-						JOptionPane.showMessageDialog(new JPanel(), "The drug ("+d_model.getModel(Drug.PROPERTY_NAME).getString()+")\nhas no ATC code associated", "Warning", JOptionPane.WARNING_MESSAGE);
-					}
-				} catch (Exception e) {
-					JOptionPane.showMessageDialog(new JPanel(), "Couldn't retrieve ATC code...", e.getMessage(), JOptionPane.ERROR_MESSAGE);
-					e.printStackTrace();
-				}
+					AtcCodeRetriever codeRetriever = new AtcCodeRetriever();
+					RunnableReadyModel readyModel = new RunnableReadyModel(codeRetriever);
+					new Thread(readyModel).start();
 			}
 		});
 	}
 
+	private class AtcCodeRetriever implements Runnable {
+		public void run() {
+			try {
+				d_loadButton.setIcon(ImageLoader.getIcon(FileNames.ICON_LOADING));
+				d_loadButton.setEnabled(false);
+				
+				try {
+					Thread.sleep(1500);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				d_atcCode = new AtcParser().getAtcCode(d_model.getModel(Drug.PROPERTY_NAME).getString()).getCode();
+				d_loadButton.setIcon(ImageLoader.getIcon(FileNames.ICON_SEARCH));
+				d_model.getModel(Drug.PROPERTY_ATCCODE).setValue(d_atcCode);
+				d_loadButton.setEnabled(true);
+				
+				if(d_atcCode == null) {
+					JOptionPane.showMessageDialog(new JPanel(), "The drug ("+d_model.getModel(Drug.PROPERTY_NAME).getString()+")\nhas no ATC code associated", "Warning", JOptionPane.WARNING_MESSAGE);
+				}
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(new JPanel(), "Couldn't retrieve ATC code...", e.getMessage(), JOptionPane.ERROR_MESSAGE);
+				//e.printStackTrace();
+			}
+		}
+	}
+	
 	public JComponent buildPanel() {
 		initComponents();
 		
@@ -99,7 +124,7 @@ public class AddDrugView implements ViewBuilder {
 		builder.add(d_name, cc.xy(3, 3));
 		builder.add(d_loadButton, cc.xy(5, 3));
 		builder.addLabel("ATC Code:", cc.xy(1, 5));
-		builder.add(d_atcCode, cc.xyw(3, 5, 3));
+		builder.add(d_atcCodeTextField, cc.xyw(3, 5, 3));
 		
 		return builder.getPanel();	
 	}
