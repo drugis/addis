55428e5a5e8e531911711759001c351c5848b439
Daniel Reid <osmosisch@gmail.com>
2010-09-07 13:37:38 +0200

    Added legend to relative effects plot display to indicate that white cells are clickable; added save and close button to resulting forest plot display

--- src/main/java/org/drugis/addis/gui/RelativeEffectPlotDialog.java
+++ src/main/java/org/drugis/addis/gui/RelativeEffectPlotDialog.java
@@ -21,19 +21,54 @@
 
 package org.drugis.addis.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 import org.drugis.addis.gui.components.RelativeEffectCanvas;
 import org.drugis.addis.presentation.ForestPlotPresentation;
+import org.drugis.addis.treeplot.ForestPlot;
+import org.drugis.common.gui.ImageExporter;
 
 @SuppressWarnings("serial")
 public class RelativeEffectPlotDialog extends JDialog {
-	public RelativeEffectPlotDialog(JDialog parent, ForestPlotPresentation pres, String title) { 
+	public RelativeEffectPlotDialog(final JDialog parent, ForestPlotPresentation pres, String title) { 
 		super(parent, title);
-		add(new RelativeEffectCanvas(pres));
+
+		JPanel panel = new JPanel(new BorderLayout());
+
+		JButton closeButton = new JButton("Close");
+		closeButton.setMnemonic('c');
+		closeButton.addActionListener(new AbstractAction() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				dispose();
+			}
+		});
+
+		final RelativeEffectCanvas canvas = new RelativeEffectCanvas(pres);
+
+		JButton saveButton = new JButton("Save Image");
+		saveButton.addActionListener(new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				ForestPlot plot = canvas.getPlot();
+				ImageExporter.writeImage(parent, plot, (int) plot.getSize().getWidth(),(int) plot.getSize().getHeight());
+			}
+		});
+		
+		panel.add(canvas, BorderLayout.NORTH);
+
+		panel.add(saveButton, BorderLayout.CENTER);
+		panel.add(closeButton, BorderLayout.SOUTH);
+		panel.setBackground(Color.WHITE);
+		
 		getComponent(0).setBackground(Color.WHITE);
+		setContentPane(panel);
 		pack();
 	}	
 }
