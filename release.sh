9fac0263a43f7931c315567dbabe31e7eccc4b9f
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-09-09 10:37:08 +0200

    Add release HOWTO file, and modify release.sh not to include data files (it is now already in the JAR).

--- release.sh
+++ release.sh
@@ -2,14 +2,15 @@
 
 VERSION=$1
 DIR=addis-$VERSION
-DATA=$DIR/data
+#DATA=$DIR/data
 
-mkdir -p $DATA
+mkdir $DIR
+#mkdir -p $DATA
 
 cp target/addis-$VERSION-jar-with-dependencies.jar $DIR/addis-$VERSION.jar
 cp LICENSE.txt $DIR
 cp README.txt $DIR
-cp hansen.xml $DATA
-cp hansen-analyses.xml $DATA 
+#cp hansen.xml $DATA
+#cp hansen-analyses.xml $DATA 
 
 zip -r addis-$VERSION.zip $DIR
