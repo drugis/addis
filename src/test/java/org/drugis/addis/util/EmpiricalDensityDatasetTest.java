3401c8ac6562e4404557ea940cd072a848d3a70c
Florin Schimbinschi <florinsch@gmail.com>
2010-12-01 11:23:40 +0100

    Add resources forgotten yesterday, fix warnings

--- src/test/java/org/drugis/addis/util/EmpiricalDensityDatasetTest.java
+++ src/test/java/org/drugis/addis/util/EmpiricalDensityDatasetTest.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 import org.apache.commons.math.stat.descriptive.summary.Sum;
