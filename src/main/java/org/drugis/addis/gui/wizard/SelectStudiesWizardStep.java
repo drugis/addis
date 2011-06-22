c9e71ed1b56ef61b603c8a3b7e95a2ef83b4e7d0
Daan Reid <osmosisch@gmail.com>
2011-06-22 16:24:01 +0200

    Simplified layout of study selection display in NMA wizard (fixed #291)

--- src/main/java/org/drugis/addis/gui/wizard/SelectStudiesWizardStep.java
+++ src/main/java/org/drugis/addis/gui/wizard/SelectStudiesWizardStep.java
@@ -25,9 +25,8 @@
 package org.drugis.addis.gui.wizard;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 
-import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 
 import org.drugis.addis.gui.AddisWindow;
@@ -37,9 +36,6 @@ import org.drugis.addis.presentation.wizard.AbstractMetaAnalysisWizardPM;
 import org.pietschy.wizard.PanelWizardStep;
 
 import com.jgoodies.binding.adapter.BasicComponentFactory;
-import com.jgoodies.forms.builder.PanelBuilder;
-import com.jgoodies.forms.layout.CellConstraints;
-import com.jgoodies.forms.layout.FormLayout;
 
 @SuppressWarnings("serial")
 public class SelectStudiesWizardStep extends PanelWizardStep {
@@ -47,31 +43,11 @@ public class SelectStudiesWizardStep extends PanelWizardStep {
 	public SelectStudiesWizardStep(AbstractMetaAnalysisWizardPM<?> pm, AddisWindow mainWindow) {
 		super("Select Studies","Select the studies to be used for meta analysis. At least one study must be selected to continue.");
 
-		setLayout(new BorderLayout());
-		JComponent studiesComp;			
+		final EnhancedTable table = EnhancedTable.createWithSorterAndAutoSize(new SelectableStudyCharTableModel(pm.getStudyListModel(), mainWindow.getPresentationModelFactory()));
 
-		EnhancedTable table = EnhancedTable.createWithSorterAndAutoSize(new SelectableStudyCharTableModel(pm.getStudyListModel(), mainWindow.getPresentationModelFactory()));
-
-		JScrollPane sPane = new JScrollPane(table);
-		sPane.getVerticalScrollBar().setUnitIncrement(16);			
-		sPane.setPreferredSize(new Dimension(700,300));
-
-		studiesComp = sPane;
-
-		FormLayout layout = new FormLayout(
-				"center:pref:grow",
-				"p, 3dlu, p"
-		);	
-
-		PanelBuilder builder = new PanelBuilder(layout);
-		builder.setDefaultDialogBorder();
-		CellConstraints cc = new CellConstraints();
-
-		builder.add(BasicComponentFactory.createLabel(pm.getStudiesMeasuringLabelModel()),
-				cc.xy(1, 1));
-		builder.add(studiesComp, cc.xy(1, 3));
-		JScrollPane sp = new JScrollPane(builder.getPanel());
-		sp.getVerticalScrollBar().setUnitIncrement(16);
-		add(sp);
+		setLayout(new BorderLayout(0, 5));
+		JLabel label = BasicComponentFactory.createLabel(pm.getStudiesMeasuringLabelModel());
+		add(label, BorderLayout.NORTH);
+		add(new JScrollPane(table), BorderLayout.CENTER);
 	}
 }
