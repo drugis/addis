18b3670d0c1be09f78cbda20e1be683bfb51ecf1
Daan Reid <osmosisch@gmail.com>
2011-11-02 16:09:28 +0100

    Fixed version info

--- application/src/main/java/org/drugis/addis/entities/Study.java
+++ application/src/main/java/org/drugis/addis/entities/Study.java
@@ -2,10 +2,10 @@
  * This file is part of ADDIS (Aggregate Data Drug Information System).
  * ADDIS is distributed from http://drugis.org/.
  * Copyright (C) 2009 Gert van Valkenhoef, Tommi Tervonen.
- * Copyright (C) 2010 Gert van Valkenhoef, Tommi Tervonen,
- * Tijs Zwinkels, Maarten Jacobs, Hanno Koeslag, Florin Schimbinschi,
+ * Copyright (C) 2010 Gert van Valkenhoef, Tommi Tervonen, 
+ * Tijs Zwinkels, Maarten Jacobs, Hanno Koeslag, Florin Schimbinschi, 
  * Ahmad Kamal, Daniel Reid.
- * Copyright (C) 2011 Gert van Valkenhoef, Ahmad Kamal,
+ * Copyright (C) 2011 Gert van Valkenhoef, Ahmad Kamal, 
  * Daniel Reid, Florin Schimbinschi.
  *
  * This program is free software: you can redistribute it and/or modify
