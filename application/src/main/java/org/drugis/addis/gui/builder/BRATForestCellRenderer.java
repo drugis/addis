8f98f44ad71814dd61bc35c712344fb0bc4d53bd
Joel Kuiper <me@joelkuiper.eu>
2012-05-07 09:49:30 +0200

    Fixed #502 [REGRESSION] Cannot copy the Forest Plots as an image

--- application/src/main/java/org/drugis/addis/gui/builder/BRATForestCellRenderer.java
+++ application/src/main/java/org/drugis/addis/gui/builder/BRATForestCellRenderer.java
@@ -44,7 +44,58 @@ import org.drugis.addis.forestplot.RelativeEffectBar;
 import org.drugis.addis.presentation.ForestPlotPresentation;
 import org.drugis.addis.presentation.BRATTableModel.BRATForest;
 
-final class BRATForestCellRenderer<PresentationType> extends DefaultTableCellRenderer {
+public class BRATForestCellRenderer<PresentationType> extends DefaultTableCellRenderer {
+	public static final class ForestPlotTableCell extends JPanel {
+		private final Color d_fg;
+		private final BRATForest d_forest;
+		private final Color d_bg;
+		private static final long serialVersionUID = 1L;
+
+		public ForestPlotTableCell(BRATForest forest, Color bg, Color fg) {
+			d_forest = forest;
+			d_bg = bg;
+			d_fg = fg;
+		}
+
+		@Override
+		public void paintComponent(Graphics g) {
+			g.setColor(d_bg);
+			Rectangle bounds = g.getClipBounds();
+			g.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
+			
+			if (d_forest == null || d_forest.scale == null) {
+				return;
+			}
+			g.translate(PADDING, 0);
+			g.setColor(d_fg);
+			if (d_forest.ci != null) {
+				final RelativeEffectBar bar = new RelativeEffectBar(d_forest.scale, ForestPlot.ROWVCENTER, d_forest.ci, ForestPlot.ROWHEIGHT / 3, d_forest.vt instanceof ContinuousVariableType);
+				bar.paint((Graphics2D) g);
+				int originX = d_forest.scale.getBin(d_forest.scale.getScale() instanceof LogScale ? 1D : 0D).bin;
+				g.drawLine(originX, 1 - ForestPlot.ROWPAD, originX, ForestPlot.FULLROW);
+			} else if (d_forest.axis != null) {
+				g.drawLine(d_forest.scale.getBin(d_forest.axis.getMin()).bin, 1, d_forest.scale.getBin(d_forest.axis.getMax()).bin, 1);
+				ForestPlot.drawAxisTicks(g, 1, ForestPlotPresentation.getTicks(d_forest.scale, d_forest.axis), ForestPlotPresentation.getTickVals(d_forest.scale, d_forest.axis));
+			}
+			g.translate(-PADDING, 0);
+		}
+
+		@Override
+		public Dimension getSize() {
+			return new Dimension(ForestPlot.BARWIDTH, ForestPlot.ROWHEIGHT);
+		}
+
+		@Override
+		public Dimension getPreferredSize() {
+			return getSize();
+		}
+
+		@Override
+		public Dimension getMinimumSize() {
+			return getSize();
+		}
+	}
+
 	private static final long serialVersionUID = -6339099621262904161L;
 	public static final int PADDING = 20;
 
@@ -55,47 +106,7 @@ final class BRATForestCellRenderer<PresentationType> extends DefaultTableCellRen
 		final Color fg = superRenderer.getForeground();
 		
 		final BRATForest forest = (BRATForest) value;
-		JPanel panel = new JPanel() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void paintComponent(Graphics g) {
-				g.setColor(bg);
-				Rectangle bounds = g.getClipBounds();
-				g.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
-				
-				if (forest == null || forest.scale == null) {
-					return;
-				}
-				g.translate(PADDING, 0);
-				g.setColor(fg);
-				if (forest.ci != null) {
-					final RelativeEffectBar bar = new RelativeEffectBar(forest.scale, ForestPlot.ROWVCENTER, forest.ci, ForestPlot.ROWHEIGHT / 3, forest.vt instanceof ContinuousVariableType);
-					bar.paint((Graphics2D) g);
-					int originX = forest.scale.getBin(forest.scale.getScale() instanceof LogScale ? 1D : 0D).bin;
-					g.drawLine(originX, 1 - ForestPlot.ROWPAD, originX, ForestPlot.FULLROW);
-				} else if (forest.axis != null) {
-					g.drawLine(forest.scale.getBin(forest.axis.getMin()).bin, 1, forest.scale.getBin(forest.axis.getMax()).bin, 1);
-					ForestPlot.drawAxisTicks(g, 1, ForestPlotPresentation.getTicks(forest.scale, forest.axis), ForestPlotPresentation.getTickVals(forest.scale, forest.axis));
-				}
-				g.translate(-PADDING, 0);
-			}
-			
-			@Override
-			public Dimension getSize() {
-				return new Dimension(ForestPlot.BARWIDTH, ForestPlot.ROWHEIGHT);
-			}
-			
-			@Override
-			public Dimension getPreferredSize() {
-				return getSize();
-			}
-			
-			@Override
-			public Dimension getMinimumSize() {
-				return getSize();
-			}
-		};
+		JPanel panel = new ForestPlotTableCell(forest, bg, fg);
 		
 		return panel;
 	}
