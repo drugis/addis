383b9c0bf1183f550bcd46f55da318c5339c71af
Joel Kuiper <me@joelkuiper.eu>
2012-05-25 15:51:57 +0200

    Put simulation status in a pane, now shows proper progress. Fixes #118

--- application/src/main/java/org/drugis/addis/gui/AnalysisComponentFactory.java
+++ application/src/main/java/org/drugis/addis/gui/AnalysisComponentFactory.java
@@ -26,7 +26,6 @@
 
 package org.drugis.addis.gui;
 
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -39,20 +38,12 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JProgressBar;
-import javax.swing.SwingConstants;
+import javax.swing.JToggleButton;
 
-import org.apache.commons.lang.StringUtils;
 import org.drugis.addis.FileNames;
 import org.drugis.addis.entities.mtcwrapper.MCMCModelWrapper;
-import org.drugis.addis.gui.components.progressgraph.GraphBar;
-import org.drugis.addis.gui.components.progressgraph.GraphConnector;
-import org.drugis.addis.gui.components.progressgraph.GraphLine;
-import org.drugis.addis.gui.components.progressgraph.GraphProgressNode;
-import org.drugis.addis.gui.components.progressgraph.GraphSimpleNode;
-import org.drugis.addis.gui.components.progressgraph.GraphSimpleNode.GraphSimpleNodeType;
+import org.drugis.addis.gui.components.progressgraph.ProgressGraph;
 import org.drugis.common.gui.task.TaskProgressBar;
-import org.drugis.common.gui.task.TaskProgressModel;
 import org.drugis.common.threading.Task;
 import org.drugis.common.threading.ThreadHandler;
 import org.drugis.common.threading.activity.ActivityTask;
@@ -69,7 +60,6 @@ import com.jgoodies.binding.value.ValueModel;
 import com.jgoodies.forms.builder.PanelBuilder;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.Sizes;
 
 public class AnalysisComponentFactory {
 	
@@ -81,7 +71,7 @@ public class AnalysisComponentFactory {
 			final JButton ... buttons) {
 
 		final FormLayout layout = new FormLayout(
-				"pref, 3dlu, fill:0:grow, 3dlu, pref",
+				"pref, 3dlu, fill:0:grow, 3dlu, pref, 3dlu, pref",
 				"p, 3dlu, p, 3dlu, p, 3dlu, p");
 		CellConstraints cc = new CellConstraints();
 		PanelBuilder panelBuilder = new PanelBuilder(layout);
@@ -90,15 +80,16 @@ public class AnalysisComponentFactory {
 			panelBuilder.addSeparator(model.toString(), cc.xyw(1, panelRow, 5));
 			panelRow += 2;
 		}
-		
-		createProgressBarRow(model, parent, cc, panelBuilder, panelRow, true, buttons, activeTab);
+		JPanel progressGraph = new ProgressGraph(model, parent).createPanel();
+		panelBuilder.add(createShowProgressGraph(model, progressGraph), cc.xy(7, panelRow));
+		createProgressBarRow(model, parent, cc, panelBuilder, panelRow, buttons, activeTab);
 		panelRow += 2;
-		if(!model.hasSavedResults() && hasConvergence(model)) { 
+		if(!model.hasSavedResults()) { 
 			panelBuilder.add(questionPanel(model), cc.xyw(1, panelRow, 3));
-		}
-		
-		panelBuilder.add(createProgressPanel(model), cc.xyw(1, panelRow + 2, 5));
-		
+		}		
+		panelRow += 2;
+
+		panelBuilder.add(progressGraph, cc.xyw(1, panelRow, 5));
 		return panelBuilder.getPanel();
 	}
 
@@ -124,87 +115,12 @@ public class AnalysisComponentFactory {
 		
 		return questionPanel;
 	}
-
-	private static JPanel createProgressPanel(final MCMCPresentation model) {
-		final int cellHeight = (int)new JProgressBar().getPreferredSize().getHeight() + 4;
-		final Dimension gridCellSize = new Dimension(95, cellHeight);
-		final int circleDiameter = 20;
-		final int edgeLength = 35;
-		final int arrowSize = 10;
-		final int barWidth = 9;
-		final int numberOfChains = model.getWrapper().getSettings().getNumberOfChains();
-		final int numMainRows = (numberOfChains - 1) * 2 + 1;
-		final int numTotalRows = numMainRows + 2;
-		final int numCols = 17;
-		
-		final FormLayout layout = new FormLayout(
-				createFormSpec("pref", numCols),
-				"p, " + createFormSpec("3dlu, p", numTotalRows - 1));
-		CellConstraints cc = new CellConstraints();
-		JPanel progressPanel = new JPanel(layout);
-
-		for(int i = 0; i < numberOfChains; ++i) {
-			int rowIdx = (2 * i) + 1;
-			Task tuningTask = getTaskByName(model.getModel().getActivityTask(), MCMCModel.TUNING_CHAIN_PREFIX + i);
-			progressPanel.add(new GraphLine(new Dimension(edgeLength, arrowSize), 2, SwingConstants.EAST), cc.xy(6, rowIdx));
-			progressPanel.add(new GraphProgressNode(gridCellSize, new TaskProgressModel(tuningTask), tuningTask.toString()), cc.xy(7, rowIdx));
-			Task simulationTask = getTaskByName(model.getModel().getActivityTask(), MCMCModel.SIMULATION_CHAIN_PREFIX + i);
-			progressPanel.add(new GraphLine(new Dimension(edgeLength * 2, arrowSize), 2, SwingConstants.EAST), cc.xy(9, rowIdx));
-			progressPanel.add(new GraphProgressNode(gridCellSize,  new TaskProgressModel(simulationTask), simulationTask.toString()), cc.xy(10, rowIdx));
-			progressPanel.add(new GraphLine(new Dimension(edgeLength, arrowSize), 2, SwingConstants.EAST), cc.xy(11, rowIdx));	
-		}
-		
-		/** Placement needed for the calculated preferred size */
-		progressPanel.add(new GraphSimpleNode(new Dimension(circleDiameter,circleDiameter), GraphSimpleNodeType.START), centerCell(cc, numMainRows, 1));
-		progressPanel.add(new GraphLine(new Dimension(edgeLength, arrowSize), 2, SwingConstants.EAST), centerCell(cc, numMainRows, 2));
-		progressPanel.add(new GraphProgressNode(gridCellSize, model.getProgressModel(), "build model", false), centerCell(cc, numMainRows, 3));
-		progressPanel.add(new GraphLine(new Dimension(edgeLength, arrowSize), 2, SwingConstants.EAST), centerCell(cc, numMainRows, 4));
-		//NOTE: it is a mystery why numMainRows - 1 is the correct count instead of just numMainRows
-		progressPanel.add(new GraphBar(new Dimension(barWidth, (int)progressPanel.getPreferredSize().getHeight())), centerCell(cc, numMainRows - 1, 5));
-		progressPanel.add(new GraphBar(new Dimension(barWidth, (int)progressPanel.getPreferredSize().getHeight())), centerCell(cc, numMainRows - 1, 12));
-		
-		Task assessConvergence = getTaskByName(model.getModel().getActivityTask(), MCMCModel.ASSESS_CONVERGENCE_PHASE);
-		
-		progressPanel.add(new GraphLine(new Dimension(edgeLength, arrowSize), 2, SwingConstants.EAST), centerCell(cc, numMainRows, 13));
-		progressPanel.add(new GraphProgressNode(gridCellSize, new TaskProgressModel(assessConvergence), assessConvergence.toString(), false), centerCell(cc, numMainRows, 14));
-		progressPanel.add(new GraphLine(new Dimension(arrowSize, 50), 2, SwingConstants.SOUTH), cc.xywh(14, numMainRows / 2 + 2, 1,  numMainRows / 2 + 1, CellConstraints.CENTER, CellConstraints.BOTTOM));
-		progressPanel.add(new GraphSimpleNode(new Dimension(circleDiameter,circleDiameter), GraphSimpleNodeType.DECISION), cc.xywh(14, numMainRows + 2, 1, 1, CellConstraints.CENTER, CellConstraints.CENTER));
-		progressPanel.add(new GraphLine(new Dimension(edgeLength + 4, arrowSize), 2, SwingConstants.EAST), cc.xyw(14, numMainRows + 2, 2, CellConstraints.RIGHT, CellConstraints.DEFAULT));
-		progressPanel.add(new GraphSimpleNode(new Dimension(circleDiameter,circleDiameter), GraphSimpleNodeType.END), cc.xy(16, numMainRows + 2));
-		progressPanel.add(new GraphLine(new Dimension(edgeLength * 6 + 2, arrowSize), 2, SwingConstants.WEST), cc.xyw(10, numMainRows + 2, 14 - 7, CellConstraints.LEFT, CellConstraints.DEFAULT));
-		progressPanel.add(new GraphBar(new Dimension(edgeLength * 2, barWidth)), cc.xy(9, numMainRows + 2));
-
-		int totalHeight = (int)progressPanel.getPreferredSize().getHeight();
-		progressPanel.add(new GraphConnector(new Dimension(edgeLength * 2, totalHeight), cellHeight + Sizes.DLUY3.getPixelSize(progressPanel), totalHeight - 30, numberOfChains), cc.xywh(9, 1, 1, numTotalRows));
-		return progressPanel;
-	}
-
-	private static String createFormSpec(String rowSpec, final int numRows) {
-		String[] rowArray = new String[numRows];
-		Arrays.fill(rowArray, rowSpec);
-		String completeRowSpec = StringUtils.join(rowArray, ",");
-		return completeRowSpec;
-	}
-
-	private static CellConstraints centerCell(CellConstraints cc, int rowSpan, int col) {
-		return cc.xywh(col, 1, 1, rowSpan, CellConstraints.CENTER, CellConstraints.CENTER);
-	}
-	
-	private static Task getTaskByName(ActivityTask task, String name) { 
-		for (Task t : task.getModel().getStates()) { 		
-			if(name.equals(t.toString())) { 
-				return t;
-			}
-		}
-		throw new IllegalArgumentException("Could not find task with name " + name);
-	}
 	
 	private static void createProgressBarRow(final MCMCPresentation model,
 			final JFrame main, 
 			final CellConstraints cc,
 			final PanelBuilder panelBuilder, 
 			final int panelRow, 
-			final boolean hasConvergence, 
 			final JButton[] buttons,
 			final String activeTab) {
 		List<JButton> buttonList = new ArrayList<JButton>(Arrays.asList(buttons));
@@ -218,12 +134,8 @@ public class AnalysisComponentFactory {
 		}
 		panelBuilder.add(bb, cc.xy(1, panelRow));
 
-		if(hasConvergence) { 
-			panelBuilder.add(new TaskProgressBar(model.getProgressModel()), cc.xy(3, panelRow));
-			panelBuilder.add(createShowConvergenceButton(main, model), cc.xy(5, panelRow));
-		} else {
-			panelBuilder.add(new TaskProgressBar(model.getProgressModel()), cc.xyw(3, panelRow, 3));
-		}
+		panelBuilder.add(new TaskProgressBar(model.getProgressModel()), cc.xy(3, panelRow));
+		panelBuilder.add(createShowConvergenceButton(main, model), cc.xy(5, panelRow));
 	}
 
 	private static JButton createRestartButton(final MCMCPresentation model, final String activeTab, final JFrame main) {
@@ -242,12 +154,13 @@ public class AnalysisComponentFactory {
 		}
 		return button;
 	}
+
 	
 	public static JButton createStartButton(final MCMCPresentation model) {
 		final JButton button = new JButton(Main.IMAGELOADER.getIcon(FileNames.ICON_RUN));
 		button.setToolTipText("Run simulation");
 		
-		if (model.getModel() == null) {
+		if (model.getWrapper().isSaved()) {
 			button.setEnabled(false);
 			return button;
 		}
@@ -265,7 +178,25 @@ public class AnalysisComponentFactory {
 		return button;
 	}
 	
+	
+	public static JToggleButton createShowProgressGraph(final MCMCPresentation model, final JPanel progressGraph) {
+		final JToggleButton button = new JToggleButton(Main.IMAGELOADER.getIcon(FileNames.ICON_CURVE_ORGANIZATION));
+		button.setToolTipText("Show simulation model");
+		
+		if (model.getWrapper().isSaved()) {
+			button.setEnabled(false);
+			return button;
+		}
+		progressGraph.setVisible(button.isSelected());
 
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				progressGraph.setVisible(button.isSelected());
+			}
+		});
+		
+		return button;
+	}
 	
 	public static JButton createStopButton(final Task task, final MCMCPresentation presentation) {
 		final JButton button = new JButton(Main.IMAGELOADER.getIcon(FileNames.ICON_TICK));
@@ -309,9 +240,5 @@ public class AnalysisComponentFactory {
 			}
 		});
 		return button;
-	}
-
-	private static boolean hasConvergence(MCMCPresentation model) {
-		return true;
 	}
 }
--- application/src/main/java/org/drugis/addis/gui/components/progressgraph/GraphProgressNode.java
+++ application/src/main/java/org/drugis/addis/gui/components/progressgraph/GraphProgressNode.java
@@ -14,6 +14,10 @@ import javax.swing.SwingConstants;
 
 import org.drugis.common.gui.task.TaskProgressBar;
 import org.drugis.common.gui.task.TaskProgressModel;
+import org.drugis.common.threading.Task;
+import org.drugis.common.threading.TaskListener;
+import org.drugis.common.threading.event.TaskEvent;
+import org.drugis.common.threading.event.TaskEvent.EventType;
 
 public class GraphProgressNode extends GraphComponent implements SwingConstants {
 	private static final long serialVersionUID = 7151331776919970759L;
@@ -21,37 +25,51 @@ public class GraphProgressNode extends GraphComponent implements SwingConstants 
 	private static final double DEFAULT_ROUNDING_ARCH = 5;
 
 	private String d_labelText;
-	private final TaskProgressModel d_progressModel;
-
 	private final boolean d_hasProgress;
+
+	private Color d_color = Color.LIGHT_GRAY;
 	
-	public GraphProgressNode(Dimension gridCellSize, TaskProgressModel progressModel, String labelText) {
-		this(gridCellSize, DEFAULT_LINE_WIDTH, DEFAULT_COLOR, progressModel, labelText);
+	public GraphProgressNode(Dimension gridCellSize, Task task) {
+		this(gridCellSize, DEFAULT_LINE_WIDTH, DEFAULT_COLOR, task);
 	}
 	
-	public GraphProgressNode(Dimension gridCellSize, int lineWidth, Color color, TaskProgressModel progressModel, String labelText) {
-		this(gridCellSize, lineWidth, color, progressModel, labelText, true);
-	}
-	
-	
-	public GraphProgressNode(Dimension gridCellSize, TaskProgressModel progressModel, String labelText, boolean hasProgress) {
-		this(gridCellSize, DEFAULT_LINE_WIDTH, DEFAULT_COLOR, progressModel, labelText, hasProgress);
+	public GraphProgressNode(Dimension gridCellSize, int lineWidth, Color color, Task task) {
+		this(gridCellSize, lineWidth, color, task, true);
 	}
 	
 	
-	public GraphProgressNode(Dimension gridCellSize, int lineWidth, Color color, TaskProgressModel progressModel, String labelText, boolean hasProgress) {
+	public GraphProgressNode(Dimension gridCellSize, Task task, boolean hasProgress) {
+		this(gridCellSize, DEFAULT_LINE_WIDTH, DEFAULT_COLOR, task, hasProgress);
+	}
+	
+	
+	public GraphProgressNode(Dimension gridCellSize, int lineWidth, Color color, Task task, boolean hasProgress) {
 		super(gridCellSize, lineWidth, color);
-		
-		d_labelText = labelText;
-		d_progressModel = progressModel;
+		d_labelText = task.toString();
 		d_hasProgress = hasProgress;
 		if(hasProgress) { 
-			TaskProgressBar tpb = new TaskProgressBar(d_progressModel);			
+			TaskProgressBar tpb = new TaskProgressBar(new TaskProgressModel(task));			
 			setLayout(new BorderLayout(0, 0));
 			add(tpb, BorderLayout.NORTH);
 			tpb.setVisible(true);
 			revalidate();
-		}	
+		}
+		task.addTaskListener(new TaskListener() {
+			
+			@Override
+			public void taskEvent(TaskEvent event) {
+				if(event.getType() == EventType.TASK_FINISHED) {
+					d_color = Color.decode("#D7FF96");
+				} else if(event.getType() == EventType.TASK_STARTED) {
+					d_color = Color.decode("#7DA6FF");
+				} else if(event.getType() == EventType.TASK_FAILED || event.getType() == EventType.TASK_ABORTED) {
+					d_color = Color.decode("#C4000D");
+				} else if(event.getType() == EventType.TASK_RESTARTED) {
+					d_color = Color.LIGHT_GRAY;
+				}
+				GraphProgressNode.this.repaint();
+			}
+		});
 	}
 
 
@@ -64,7 +82,7 @@ public class GraphProgressNode extends GraphComponent implements SwingConstants 
 			Dimension s = getPreferredSize();
 			g2.setStroke(new BasicStroke((float)d_lineWidth));
 			RoundRectangle2D.Double s2 = new RoundRectangle2D.Double(d_lineWidth / 2.0, d_lineWidth / 2.0, s.getWidth() - d_lineWidth, s.getHeight() - d_lineWidth, DEFAULT_ROUNDING_ARCH, DEFAULT_ROUNDING_ARCH);
-			g2.setPaint(Color.YELLOW);
+			g2.setPaint(d_color);
 			g2.fill(s2);
 			g2.setPaint(DEFAULT_COLOR);
 			g2.draw(s2);
