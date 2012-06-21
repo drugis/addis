f556babcc5ef07c5fe9189463d635136d6e4464b
Joel Kuiper <me@joelkuiper.eu>
2012-06-21 14:21:03 +0200

    -a
    Changed dialogue for DosedDrugTreatment to Wizard, story #535

---
+++ application/src/main/java/org/drugis/addis/gui/AddDosedDrugTreatmentWizard.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of ADDIS (Aggregate Data Drug Information System).
+ * ADDIS is distributed from http://drugis.org/.
+ * Copyright (C) 2009 Gert van Valkenhoef, Tommi Tervonen.
+ * Copyright (C) 2010 Gert van Valkenhoef, Tommi Tervonen, 
+ * Tijs Zwinkels, Maarten Jacobs, Hanno Koeslag, Florin Schimbinschi, 
+ * Ahmad Kamal, Daniel Reid.
+ * Copyright (C) 2011 Gert van Valkenhoef, Ahmad Kamal, 
+ * Daniel Reid, Florin Schimbinschi.
+ * Copyright (C) 2012 Gert van Valkenhoef, Daniel Reid, 
+ * Joël Kuiper, Wouter Reckman.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.drugis.addis.gui;
+
+import javax.swing.JDialog;
+
+import org.drugis.addis.entities.Domain;
+import org.drugis.addis.gui.wizard.AddDosedDrugTreatmentWizardStep;
+import org.drugis.addis.presentation.DosedDrugTreatmentPresentation;
+import org.pietschy.wizard.Wizard;
+import org.pietschy.wizard.WizardModel;
+import org.pietschy.wizard.models.StaticModel;
+
+@SuppressWarnings("serial")
+public class AddDosedDrugTreatmentWizard extends Wizard {
+
+	public AddDosedDrugTreatmentWizard(DosedDrugTreatmentPresentation pm, AddisWindow mainWindow, Domain domain, JDialog dialog) {
+		super(buildModel(pm, mainWindow, domain, dialog));
+
+	}
+
+	private static WizardModel buildModel(DosedDrugTreatmentPresentation pm, AddisWindow mainWindow, Domain domain, JDialog dialog) {
+		StaticModel wizardModel = new StaticModel();
+		wizardModel.add(new AddDosedDrugTreatmentWizardStep(pm, domain, mainWindow));
+		return wizardModel;
+	}
+	
+}
