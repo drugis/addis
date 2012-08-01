63a8402ddac3f320c51e815f30234247dd91644f
Joel Kuiper <me@joelkuiper.eu>
2012-08-01 15:40:11 +0200

    Add XML save and load of TreatmentCategorizations
    Story #552, no tests yet and no appropriate default data. Save and load works. Use XLST to transform default data.
    
    Signed-off-by: Joel Kuiper <me@joelkuiper.eu>

---
+++ application/src/test/java/org/drugis/addis/util/convertors/TreatmentCategorizationsConverterTest.java
@@ -0,0 +1,18 @@
+package org.drugis.addis.util.convertors;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TreatmentCategorizationsConverterTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
