53550722627f1cb49f11da2a7c833aebead43bcc
Joel Kuiper <me@joelkuiper.eu>
2012-08-15 09:30:40 +0200

    Revert "Comment out uncompiling PairWiseMetaAnalysis for development release"
    This reverts commit c3b644ce695c22dc22de39619869e454482e504f.

--- application/src/main/java/org/drugis/addis/gui/wizard/PairwiseMetaAnalysisWizard.java
+++ application/src/main/java/org/drugis/addis/gui/wizard/PairwiseMetaAnalysisWizard.java
@@ -61,7 +61,7 @@ import com.jgoodies.forms.layout.FormLayout;
 
 @SuppressWarnings("serial")
 public class PairwiseMetaAnalysisWizard extends Wizard {
-
+	
 	public PairwiseMetaAnalysisWizard(AddisWindow mainWindow, PairWiseMetaAnalysisWizardPresentation pm) {
 		super(buildModel(pm, mainWindow));
 		setDefaultExitMode(Wizard.EXIT_ON_FINISH);
@@ -69,122 +69,122 @@ public class PairwiseMetaAnalysisWizard extends Wizard {
 	
 	private static WizardModel buildModel(PairWiseMetaAnalysisWizardPresentation pm, AddisWindow mainWindow) {
 		StaticModel wizardModel = new StaticModel();
-//		SelectIndicationAndNameWizardStep selectIndicationStep = new SelectIndicationAndNameWizardStep(pm, mainWindow);
-//		wizardModel.add(selectIndicationStep);
-//		wizardModel.add(new SelectEndpointWizardStep(pm));
-//		wizardModel.add(new SelectDrugsWizardStep(pm, mainWindow));
-//		SelectStudiesWizardStep selectStudiesStep = new SelectStudiesWizardStep(pm);
-//		wizardModel.add(selectStudiesStep);
-//		Bindings.bind(selectStudiesStep, "complete", pm.getMetaAnalysisCompleteModel());
-//		wizardModel.add(new SelectArmsWizardStep(pm));
-//		wizardModel.add(new OverviewWizardStep(pm, mainWindow));
+		SelectIndicationAndNameWizardStep selectIndicationStep = new SelectIndicationAndNameWizardStep(pm, mainWindow);
+		wizardModel.add(selectIndicationStep);
+		wizardModel.add(new SelectEndpointWizardStep(pm));
+		wizardModel.add(new SelectDrugsWizardStep(pm, mainWindow));
+		SelectStudiesWizardStep selectStudiesStep = new SelectStudiesWizardStep(pm);
+		wizardModel.add(selectStudiesStep);
+		Bindings.bind(selectStudiesStep, "complete", pm.getMetaAnalysisCompleteModel());
+		wizardModel.add(new SelectArmsWizardStep(pm));
+		wizardModel.add(new OverviewWizardStep(pm, mainWindow));
 		return wizardModel;
 	}
-//
-//	public static class OverviewWizardStep extends AbstractOverviewWizardStep<TreatmentDefinitionsGraphModel> {
-//		public OverviewWizardStep(PairWiseMetaAnalysisWizardPresentation pm, AddisWindow mainWindow) {
-//			super(pm, mainWindow);
-//		}
-//		
-//		@Override
-//		public void prepare() {
-//			removeAll();
-//
-//			setLayout(new BorderLayout()); // needed for placement
-//			
-//			PairWiseMetaAnalysisPresentation pm = ((PairWiseMetaAnalysisWizardPresentation)d_pm).getMetaAnalysisModel();
-//			final PairWiseMetaAnalysisView mav = new PairWiseMetaAnalysisView(
-//					(PairWiseMetaAnalysisPresentation)pm, d_mainWindow);
-//			final JComponent panel = mav.getPlotsPanel(true);
-//
-//			if(d_pm.getOutcomeMeasureModel().getValue().getVariableType() instanceof RateVariableType){
-//				JCheckBox checkBox = BasicComponentFactory.createCheckBox(pm.getModel(RandomEffectsMetaAnalysis.PROPERTY_CORRECTED), "Correct for zeroes");
-//				add(checkBox, BorderLayout.NORTH);
-//			}
-//			
-//			final JScrollPane sp = new JScrollPane(panel);
-//			sp.getVerticalScrollBar().setUnitIncrement(16);
-//			add(sp, BorderLayout.CENTER);
-//			
-//			pm.getModel(RandomEffectsMetaAnalysis.PROPERTY_CORRECTED).addValueChangeListener(new PropertyChangeListener() {
-//				public void propertyChange(PropertyChangeEvent evt) {
-//					sp.setVisible(false);
-//					sp.setViewportView(mav.getPlotsPanel(true));
-//					sp.setVisible(true);
-//				}
-//			});
-//			
-//			setComplete(true);
-//		}
-//	}
-//
-//	public static class SelectDrugsWizardStep extends PanelWizardStep {
-//		PairWiseMetaAnalysisWizardPresentation d_pm;
-//		JFrame d_frame;
-//		private StudyGraph d_studyGraph;
-//
-//		public SelectDrugsWizardStep(PairWiseMetaAnalysisWizardPresentation pm, JFrame frame) {
-//			super("Select Drugs","Select the drugs to be used for meta analysis.");
-//			
-//			d_pm = pm;
-//			d_frame = frame;
-//			setLayout(new BorderLayout());
-//			FormLayout layout = new FormLayout(
-//					"center:pref:grow",
-//					"p, 3dlu, p, 3dlu, p"
-//					);	
-//			
-//			PanelBuilder builder = new PanelBuilder(layout);
-//			builder.setDefaultDialogBorder();
-//			CellConstraints cc = new CellConstraints();
-//			
-//			builder.add(buildSelectDrugsPanel(), cc.xy(1, 1));			
-//			builder.add(BasicComponentFactory.createLabel(d_pm.getStudiesMeasuringLabelModel()),
-//					cc.xy(1, 3));
-//			builder.setBorder(BorderFactory.createEmptyBorder());
-//			builder.add(buildStudiesGraph(), cc.xy(1, 5));
-//			
-//			JScrollPane sp = new JScrollPane(builder.getPanel());
-//			add(sp);
-//			sp.getVerticalScrollBar().setUnitIncrement(16);			
-//			
-//			Bindings.bind(this, "complete", d_pm.getMetaAnalysisCompleteModel());
-//		}
-//		
-//		private Component buildStudiesGraph() {
-//			TreatmentDefinitionsGraphModel pm = d_pm.getRefinedAlternativesGraph();
-//			d_studyGraph = new StudyGraph(pm);
-//			return d_studyGraph;
-//		}
-//		
-//		@Override
-//		public void prepare() {
-//			d_pm.rebuildRefinedAlternativesGraph();
-//			d_studyGraph.layoutGraph();
-//		}
-//
-//		private JPanel buildSelectDrugsPanel() {
-//			FormLayout layout = new FormLayout(
-//					"center:pref, 3dlu, center:pref, 3dlu, center:pref",
-//					"p, 3dlu, p, 3dlu, p, 3dlu, p"
-//					);	
-//			
-//			PanelBuilder builder = new PanelBuilder(layout);
-//			builder.setDefaultDialogBorder();
-//			
-//			CellConstraints cc = new CellConstraints();
-//			builder.addLabel("First Drug",cc.xy(1, 1));
-//			builder.addLabel("Second Drug",cc.xy(5, 1));
-//						
-//			JComboBox firstDrugBox = AuxComponentFactory.createBoundComboBox(d_pm.getAvailableRawTreatmentDefinitions(), d_pm.getFirstDrugModel(), true);
-//			JComboBox secondDrugBox = AuxComponentFactory.createBoundComboBox(d_pm.getAvailableRawTreatmentDefinitions(), d_pm.getSecondDrugModel(), true);
-//			
-//			builder.add(firstDrugBox,cc.xy(1, 3));
-//			builder.add(secondDrugBox,cc.xy(5, 3));
-//			builder.addLabel("VS",cc.xy(3, 3));
-//			JPanel panel = builder.getPanel();			
-//			
-//			return panel;
-//		}
-//	}	
+
+	public static class OverviewWizardStep extends AbstractOverviewWizardStep<TreatmentDefinitionsGraphModel> {
+		public OverviewWizardStep(PairWiseMetaAnalysisWizardPresentation pm, AddisWindow mainWindow) {
+			super(pm, mainWindow);
+		}
+		
+		@Override
+		public void prepare() {
+			removeAll();
+
+			setLayout(new BorderLayout()); // needed for placement
+			
+			PairWiseMetaAnalysisPresentation pm = ((PairWiseMetaAnalysisWizardPresentation)d_pm).getMetaAnalysisModel();
+			final PairWiseMetaAnalysisView mav = new PairWiseMetaAnalysisView(
+					(PairWiseMetaAnalysisPresentation)pm, d_mainWindow);
+			final JComponent panel = mav.getPlotsPanel(true);
+
+			if(d_pm.getOutcomeMeasureModel().getValue().getVariableType() instanceof RateVariableType){
+				JCheckBox checkBox = BasicComponentFactory.createCheckBox(pm.getModel(RandomEffectsMetaAnalysis.PROPERTY_CORRECTED), "Correct for zeroes");
+				add(checkBox, BorderLayout.NORTH);
+			}
+			
+			final JScrollPane sp = new JScrollPane(panel);
+			sp.getVerticalScrollBar().setUnitIncrement(16);
+			add(sp, BorderLayout.CENTER);
+			
+			pm.getModel(RandomEffectsMetaAnalysis.PROPERTY_CORRECTED).addValueChangeListener(new PropertyChangeListener() {
+				public void propertyChange(PropertyChangeEvent evt) {
+					sp.setVisible(false);
+					sp.setViewportView(mav.getPlotsPanel(true));
+					sp.setVisible(true);
+				}
+			});
+			
+			setComplete(true);
+		}
+	}
+
+	public static class SelectDrugsWizardStep extends PanelWizardStep {
+		PairWiseMetaAnalysisWizardPresentation d_pm;
+		JFrame d_frame;
+		private StudyGraph d_studyGraph;
+
+		public SelectDrugsWizardStep(PairWiseMetaAnalysisWizardPresentation pm, JFrame frame) {
+			super("Select Drugs","Select the drugs to be used for meta analysis.");
+			
+			d_pm = pm;
+			d_frame = frame;
+			setLayout(new BorderLayout());
+			FormLayout layout = new FormLayout(
+					"center:pref:grow",
+					"p, 3dlu, p, 3dlu, p"
+					);	
+			
+			PanelBuilder builder = new PanelBuilder(layout);
+			builder.setDefaultDialogBorder();
+			CellConstraints cc = new CellConstraints();
+			
+			builder.add(buildSelectDrugsPanel(), cc.xy(1, 1));			
+			builder.add(BasicComponentFactory.createLabel(d_pm.getStudiesMeasuringLabelModel()),
+					cc.xy(1, 3));
+			builder.setBorder(BorderFactory.createEmptyBorder());
+			builder.add(buildStudiesGraph(), cc.xy(1, 5));
+			
+			JScrollPane sp = new JScrollPane(builder.getPanel());
+			add(sp);
+			sp.getVerticalScrollBar().setUnitIncrement(16);			
+			
+			Bindings.bind(this, "complete", d_pm.getMetaAnalysisCompleteModel());
+		}
+		
+		private Component buildStudiesGraph() {
+			TreatmentDefinitionsGraphModel pm = d_pm.getRefinedAlternativesGraph();
+			d_studyGraph = new StudyGraph(pm);
+			return d_studyGraph;
+		}
+		
+		@Override
+		public void prepare() {
+			d_pm.rebuildRefinedAlternativesGraph();
+			d_studyGraph.layoutGraph();
+		}
+
+		private JPanel buildSelectDrugsPanel() {
+			FormLayout layout = new FormLayout(
+					"center:pref, 3dlu, center:pref, 3dlu, center:pref",
+					"p, 3dlu, p, 3dlu, p, 3dlu, p"
+					);	
+			
+			PanelBuilder builder = new PanelBuilder(layout);
+			builder.setDefaultDialogBorder();
+			
+			CellConstraints cc = new CellConstraints();
+			builder.addLabel("First Drug",cc.xy(1, 1));
+			builder.addLabel("Second Drug",cc.xy(5, 1));
+						
+			JComboBox firstDrugBox = AuxComponentFactory.createBoundComboBox(d_pm.getAvailableRawTreatmentDefinitions(), d_pm.getFirstDrugModel(), true);
+			JComboBox secondDrugBox = AuxComponentFactory.createBoundComboBox(d_pm.getAvailableRawTreatmentDefinitions(), d_pm.getSecondDrugModel(), true);
+			
+			builder.add(firstDrugBox,cc.xy(1, 3));
+			builder.add(secondDrugBox,cc.xy(5, 3));
+			builder.addLabel("VS",cc.xy(3, 3));
+			JPanel panel = builder.getPanel();			
+			
+			return panel;
+		}
+	}	
 }
