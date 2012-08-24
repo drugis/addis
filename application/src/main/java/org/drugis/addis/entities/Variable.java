8532155282468133e795c79f9d2e6778745c531c
Joel Kuiper <me@joelkuiper.eu>
2012-08-24 17:05:23 +0200

    Fix #554 BR: measurements conformant to EMA "effects table"
    Signed-off-by: Joel Kuiper <me@joelkuiper.eu>

--- application/src/main/java/org/drugis/addis/entities/Variable.java
+++ application/src/main/java/org/drugis/addis/entities/Variable.java
@@ -33,17 +33,17 @@ public interface Variable extends Entity, Comparable<Variable>, TypeWithName {
 		CONTINUOUS("Continuous"),
 		RATE("Rate"),
 		CATEGORICAL("Categorical");
-		
-		
+
+
 		private String d_name;
-		
+
 		Type() {
 		}
-		
+
 		Type(String name) {
 			d_name = name;
 		}
-		
+
 		@Override
 		public String toString() {
 			return d_name;
@@ -52,30 +52,30 @@ public interface Variable extends Entity, Comparable<Variable>, TypeWithName {
 
 	public final static String PROPERTY_VARIABLE_TYPE = "variableType";
 	public final static String PROPERTY_DESCRIPTION = "description";
-	
-	public static final String UOM_DEFAULT_RATE = "Ratio of Patients";
+
+	public static final String UOM_DEFAULT_RATE = "Proportion of Patients";
 	public static final String UOM_DEFAULT_CONTINUOUS = "";
 
 	public void setDescription(String description);
 
 	public String getDescription();
-	
+
 	public VariableType getVariableType();
-	
+
 	public void setVariableType(VariableType type);
-	
+
 	/**
 	 * Build a Measurement on this variable.
 	 * @return An appropriate type of Measurement.
 	 */
 	public Measurement buildMeasurement();
-	
+
 	/**
 	 * Build a Measurement on this variable.
 	 * @param size Default group size
 	 * @return An appropriate type of Measurement.
 	 */
 	public BasicMeasurement buildMeasurement(int size);
-	
+
 	public void setName(String name);
 }
