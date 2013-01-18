e9616d6623fdb19bfc193c4befb2a4387e499e2b
Joel Kuiper <me@joelkuiper.eu>
2013-01-18 17:29:13 +0100

    Add tools for automatic conversion and rebuild

--- docs/dbms/rebuild.sh
+++ docs/dbms/rebuild.sh
@@ -6,6 +6,9 @@ PGPASSWORD=develop psql -h $HOST -p $PORT -U postgres -c  "SELECT pg_terminate_b
 PGPASSWORD=develop psql -h $HOST -p $PORT -U postgres -c "DROP DATABASE IF EXISTS addis"
 psql -h $HOST -p $PORT -U addis -d postgres -c "CREATE DATABASE addis ENCODING 'utf-8' OWNER addis"
 psql -h $HOST -p $PORT -U addis -f structure.sql
-#psql -h $HOST -p $PORT -U addis -f diabetes.sql
 
+./transform.sh "depressionExample" "Hansen 2005" "Depression dataset based on the Hansen et al. (2005) systematic review"
+psql -h $HOST -p $PORT -U addis -f depressionExample.sql
 
+./transform.sh "hypertensionExample" "Edarbi EPAR" "Hypertension dataset based on the Edarbi EPAR"
+psql -h $HOST -p $PORT -U addis -f hypertensionExample.sql
