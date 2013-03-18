6edc73e7b4e4a16881bf36fddf0da36cf2252400
Joël Kuiper <me@joelkuiper.eu>
2013-03-18 15:12:28 +0100

    Minor cleanup in DBMS structure
    More consistent referencing

--- docs/dbms/structure.sql
+++ docs/dbms/structure.sql
@@ -6,33 +6,27 @@ CREATE TYPE status AS ENUM ('NOT_YET_RECRUITING', 'RECRUITING', 'ENROLLING', 'AC
 CREATE TYPE measurement_type as ENUM ('CONTINUOUS', 'RATE', 'CATEGORICAL');
 CREATE TYPE variable_type as ENUM ('PopulationCharacteristic', 'Endpoint', 'AdverseEvent');
 CREATE TYPE epoch_offset as ENUM ('FROM_EPOCH_START', 'BEFORE_EPOCH_END');
+CREATE TYPE concept_type as ENUM ('DRUG', 'INDICATION', 'UNIT', 'VARIABLE');
+
 CREATE EXTENSION "uuid-ossp";
 
-CREATE TABLE "projects" (
-  "id" bigserial,
-  "name" varchar,
-  "description" text,
-  PRIMARY KEY ("id")
+CREATE TABLE "code_systems" (
+  "code_system" varchar,
+  "code_system_name" varchar,
+  PRIMARY KEY ("code_system")
 );
 
-CREATE TABLE "project_studies" (
-  "project_id" bigint REFERENCES projects (id),
-  "study_id" bigint,
-  PRIMARY KEY ("project_id", "study_id")
-);
-
-CREATE TYPE concept_type as ENUM ('DRUG', 'INDICATION', 'UNIT', 'VARIABLE');
 CREATE TABLE "concepts" (
   "id" uuid,
   "name" varchar NOT NULL,
   "description" text,
   "type" concept_type,
   "code" varchar,
-  "code_system" varchar,
+  "code_system" varchar REFERENCES code_systems (code_system),
   "owner" varchar NOT NULL,
-  PRIMARY KEY ("id")
+  PRIMARY KEY ("id"),
+  UNIQUE("code", "code_system")
 );
-CREATE UNIQUE INDEX ON "concepts" ("code", "code_system");
 
 CREATE TABLE "concept_map" (
   "sub" uuid REFERENCES concepts (id),
@@ -40,26 +34,38 @@ CREATE TABLE "concept_map" (
   PRIMARY KEY ("sub", "super")
 );
 
-CREATE TABLE "treatment_dosings" (
-  "treatment_id" bigint,
-  "planned_time" interval,
-  "min_dose" varchar,
-  "max_dose" varchar,
-  "scale_modifier" varchar,
-  "unit_concept" uuid,
-  PRIMARY KEY ("treatment_id", "planned_time")
+CREATE TABLE "note_hooks" (
+  "id" bigserial,
+  PRIMARY KEY ("id")
 );
-CREATE INDEX ON "treatment_dosings" ("treatment_id") WHERE "planned_time" IS NULL;
 
+CREATE TABLE "notes" (
+  "id" bigserial,
+  "note_hook_id" bigint REFERENCES note_hooks (id),
+  "text" text,
+  "source" source,
+  PRIMARY KEY ("id", "note_hook_id")
+);
 CREATE TABLE "treatments" (
   "id" bigserial,
   "study_id" bigint,
-  "activity_name" varchar,
-  "drug_concept" uuid,
+  "activity_name" varchar NOT NULL,
+  "drug_concept" uuid REFERENCES concepts (id),
   "periodicity" interval DEFAULT 'P0D',
   PRIMARY KEY ("id"),
   UNIQUE("study_id", "activity_name", "drug_concept")
 );
+
+CREATE TABLE "treatment_dosings" (
+  "treatment_id" bigint REFERENCES treatments (id),
+  "planned_time" interval,
+  "min_dose" varchar,
+  "max_dose" varchar,
+  "scale_modifier" varchar,
+  "unit_concept" uuid REFERENCES concepts (id),
+  PRIMARY KEY ("treatment_id", "planned_time")
+);
+CREATE INDEX ON "treatment_dosings" ("treatment_id") WHERE "planned_time" IS NULL;
 
 CREATE TABLE "activities" (
   "study_id" bigint,
@@ -67,35 +73,50 @@ CREATE TABLE "activities" (
   "type" activity_type,
   PRIMARY KEY ("study_id", "name")
 );
+ALTER TABLE "treatments" ADD CONSTRAINT "treatment_activity_fkey" FOREIGN KEY ("study_id", "activity_name") REFERENCES "activities" ("study_id", "name");
+
 CREATE TABLE "studies" (
   "id" bigserial,
-  "name" varchar,
+  "name" varchar NOT NULL,
   "title" text,
-  "indication_concept" uuid,
+  "indication_concept" uuid REFERENCES concepts (id),
   "objective" text,
   "allocation_type" allocation_type,
   "blinding_type" blinding_type,
   "number_of_centers" int2,
   "created_at" date,
-  "source" source,
+  "source" source DEFAULT 'MANUAL',
   "exclusion" text,
   "inclusion" text,
   "status" status,
   "start_date" date,
   "end_date" date,
-  "note_hook" bigint,
-  "blinding_type_note_hook" bigint,
-  "title_note_hook" bigint,
-  "allocation_type_note_hook" bigint,
+  "note_hook" bigint REFERENCES note_hooks (id),
+  "blinding_type_note_hook" bigint REFERENCES note_hooks (id),
+  "title_note_hook" bigint REFERENCES note_hooks (id),
+  "allocation_type_note_hook" bigint REFERENCES note_hooks (id),
   PRIMARY KEY ("id")
 );
 CREATE INDEX ON "studies" ("name");
 CREATE INDEX ON "studies" ("indication_concept");
 
+CREATE TABLE "projects" (
+  "id" bigserial,
+  "name" varchar NOT NULL,
+  "description" text,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "project_studies" (
+  "project_id" bigint REFERENCES projects (id),
+  "study_id" bigint REFERENCES studies (id),
+  PRIMARY KEY ("project_id", "study_id")
+);
+
 CREATE TABLE "study_references" (
   "study_id" bigint REFERENCES studies (id),
   "id" varchar,
-  "repostitory" text DEFAULT 'PubMed',
+  "repository" text DEFAULT 'PubMed',
   PRIMARY KEY ("study_id", "id")
 );
 
@@ -103,7 +124,7 @@ CREATE TABLE "epochs" (
   "study_id" bigint REFERENCES studies (id),
   "name" varchar,
   "duration" interval DEFAULT 'P0D',
-  "note_hook" bigint,
+  "note_hook" bigint REFERENCES note_hooks (id),
   PRIMARY KEY ("study_id", "name")
 );
 
@@ -111,7 +132,7 @@ CREATE TABLE "arms" (
   "study_id" bigint REFERENCES studies (id),
   "name" varchar,
   "arm_size" varchar,
-  "note_hook" bigint,
+  "note_hook" bigint REFERENCES note_hooks (id),
   PRIMARY KEY ("study_id", "name")
 );
 COMMENT ON COLUMN "arms"."name" IS 'Empty string indicates "total population"';
@@ -132,22 +153,21 @@ CREATE TABLE "study_variables" (
   "variable_concept" uuid REFERENCES concepts (id),
   "is_primary" bool,
   "measurement_type" measurement_type,
-  "unit_concept" uuid,
+  "unit_concept" uuid REFERENCES concepts (id),
   "variable_type" variable_type,
-  "note_hook" bigint,
+  "note_hook" bigint REFERENCES note_hooks (id),
   PRIMARY KEY ("study_id", "variable_concept")
 );
-CREATE INDEX ON "study_variables" ("study_id");
 
 CREATE TABLE "variable_categories" (
-  "variable_concept" uuid,
+  "variable_concept" uuid REFERENCES concepts (id),
   "category_name" varchar,
   PRIMARY KEY ("variable_concept", "category_name")
 );
 
 CREATE TABLE "measurements" (
   "study_id" bigint,
-  "variable_concept" uuid,
+  "variable_concept" uuid REFERENCES concepts (id),
   "measurement_moment_name" varchar,
   "arm_name" varchar,
   "attribute" varchar,
@@ -156,6 +176,8 @@ CREATE TABLE "measurements" (
   PRIMARY KEY ("variable_concept", "measurement_moment_name", "arm_name", "attribute")
 );
 COMMENT ON COLUMN "measurements"."variable_concept" IS 'Uniquely identifies the study';
+ALTER TABLE "measurements" ADD CONSTRAINT "variable_measurement_fkey" FOREIGN KEY ("study_id", "variable_concept") REFERENCES "study_variables" ("study_id", "variable_concept");
+ALTER TABLE "measurements" ADD CONSTRAINT "arm_measurement_fkey" FOREIGN KEY ("study_id", "arm_name") REFERENCES "arms" ("study_id", "name");
 
 CREATE TABLE "measurement_moments" (
   "study_id" bigint,
@@ -164,55 +186,9 @@ CREATE TABLE "measurement_moments" (
   "is_primary" bool,
   "offset_from_epoch" interval,
   "before_epoch" epoch_offset,
-  "note_hook" bigint,
+  "note_hook" bigint REFERENCES note_hooks (id),
   PRIMARY KEY ("study_id", "name"),
   UNIQUE ("study_id", "epoch_name", "offset_from_epoch", "before_epoch")
 );
-
-CREATE TABLE "code_systems" (
-  "code_system" varchar,
-  "code_system_name" varchar,
-  PRIMARY KEY ("code_system")
-);
-
-CREATE TABLE "note_hooks" (
-  "id" bigserial,
-  PRIMARY KEY ("id")
-);
-
-CREATE TABLE "notes" (
-  "id" bigserial,
-  "note_hook_id" bigint,
-  "text" text,
-  "source" source,
-  PRIMARY KEY ("id", "note_hook_id")
-);
-
-ALTER TABLE "project_studies" ADD CONSTRAINT "study_fkey" FOREIGN KEY ("study_id") REFERENCES "studies" ("id");
-
-ALTER TABLE "concepts" ADD CONSTRAINT "concept_code_system_fkey" FOREIGN KEY ("code_system") REFERENCES "code_systems" ("code_system");
-
-ALTER TABLE "variable_categories" ADD CONSTRAINT "variable_category_fkey" FOREIGN KEY ("variable_concept") REFERENCES "concepts" ("id");
-ALTER TABLE "study_variables" ADD CONSTRAINT "study_variable_fkey" FOREIGN KEY ("variable_concept") REFERENCES "concepts" ("id");
-ALTER TABLE "study_variables" ADD CONSTRAINT "study_variable_note_hook_fkey" FOREIGN KEY ("note_hook") REFERENCES "note_hooks" ("id");
-
 ALTER TABLE "measurement_moments" ADD CONSTRAINT "epoch_study_measurement_fkey" FOREIGN KEY ("study_id", "epoch_name") REFERENCES "epochs" ("study_id", "name");
-ALTER TABLE "measurement_moments" ADD CONSTRAINT "study_measurement_note_hook_fkey" FOREIGN KEY ("note_hook") REFERENCES "note_hooks" ("id");
-ALTER TABLE "measurements" ADD CONSTRAINT "variable_measurement_fkey" FOREIGN KEY ("study_id", "variable_concept") REFERENCES "study_variables" ("study_id", "variable_concept");
-ALTER TABLE "measurements" ADD CONSTRAINT "arm_measurement_fkey" FOREIGN KEY ("study_id", "arm_name") REFERENCES "arms" ("study_id", "name");
 ALTER TABLE "measurements" ADD CONSTRAINT "measurement_moments_fkey" FOREIGN KEY ("study_id", "measurement_moment_name") REFERENCES "measurement_moments" ("study_id", "name");
-ALTER TABLE "arms" ADD CONSTRAINT "study_arms_note_hook_fkey" FOREIGN KEY ("note_hook") REFERENCES "note_hooks" ("id");
-ALTER TABLE "epochs" ADD CONSTRAINT "study_epochs_note_hook_fkey" FOREIGN KEY ("note_hook") REFERENCES "note_hooks" ("id");
-
-ALTER TABLE "treatments" ADD CONSTRAINT "treatments_drug_fkey" FOREIGN KEY ("drug_concept") REFERENCES "concepts" ("id");
-ALTER TABLE "treatments" ADD CONSTRAINT "treatment_activity_fkey" FOREIGN KEY ("study_id", "activity_name") REFERENCES "activities" ("study_id", "name");
-ALTER TABLE "treatment_dosings" ADD CONSTRAINT "treatment_dosings_unit_fkey" FOREIGN KEY ("unit_concept") REFERENCES "concepts" ("id");
-ALTER TABLE "treatment_dosings" ADD CONSTRAINT "treatment_dosings_fkey" FOREIGN KEY ("treatment_id") REFERENCES "treatments" ("id");
-
-ALTER TABLE "notes" ADD CONSTRAINT "note_note_hooks_fkey" FOREIGN KEY ("note_hook_id") REFERENCES "note_hooks" ("id");
-
-ALTER TABLE "studies" ADD CONSTRAINT "study_note_hook_fkey" FOREIGN KEY ("note_hook") REFERENCES "note_hooks" ("id");
-ALTER TABLE "studies" ADD CONSTRAINT "study_indication_fkey" FOREIGN KEY ("indication_concept") REFERENCES "concepts" ("id");
-ALTER TABLE "studies" ADD CONSTRAINT "study_blinding_type_note_hook_fkey" FOREIGN KEY ("blinding_type_note_hook") REFERENCES "note_hooks" ("id");
-ALTER TABLE "studies" ADD CONSTRAINT "study_title_note_hook_fkey" FOREIGN KEY ("title_note_hook") REFERENCES "note_hooks" ("id");
-ALTER TABLE "studies" ADD CONSTRAINT "allocation_type_note_hook_fkey" FOREIGN KEY ("allocation_type_note_hook") REFERENCES "note_hooks" ("id");
