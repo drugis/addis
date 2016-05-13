33b2971b1f9b5064d8fc1493660f3f9d5b3a1a07
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2016-05-13 17:30:19 +0200

    Initial support for export to ADDIS 2

---
+++ application/src/main/java/org/drugis/addis/gui/Addis2ExportDialog.java
@@ -0,0 +1,157 @@
+package org.drugis.addis.gui;
+
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HttpContext;
+import org.codehaus.jackson.map.JsonDeserializer;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.drugis.addis.entities.DomainManager;
+import org.drugis.addis.gui.Addis2ExportDialog.ExportInfo;
+import org.drugis.addis.gui.builder.Addis2ExportView;
+import org.drugis.addis.gui.util.NonEmptyValueModel;
+import org.drugis.addis.presentation.ModifiableHolder;
+import org.drugis.addis.rdf.AddisToRdfFactory;
+import org.drugis.common.beans.AbstractObservable;
+import org.drugis.common.gui.OkCancelDialog;
+
+import com.jgoodies.binding.PresentationModel;
+
+@SuppressWarnings("serial")
+public class Addis2ExportDialog extends JDialog {
+	public static class ExportInfo extends AbstractObservable {
+		private String name = "";
+		private String title = "";
+		private String apiKey = "";
+		private String server = "https://addis.drugis.org/";
+		private String datasetId = UUID.randomUUID().toString();
+		
+		private String status = "";
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String newVal) {
+			String oldVal = this.name;
+			this.name = newVal;
+			firePropertyChange("name", oldVal, newVal);
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String newVal) {
+			String oldVal = this.title;
+			this.title = newVal;
+			firePropertyChange("title", oldVal, newVal);
+		}
+		public String getApiKey() {
+			return apiKey;
+		}
+		public void setApiKey(String newVal) {
+			String oldVal = this.apiKey;
+			this.apiKey = newVal;
+			firePropertyChange("apiKey", oldVal, newVal);
+		}
+		public String getServer() {
+			return server;
+		}
+		public void setServer(String newVal) {
+			String oldVal = this.server;
+			this.server = newVal;
+			firePropertyChange("server", oldVal, newVal);
+		}
+		public String getDatasetId() {
+			return datasetId;
+		}
+		public void setDatasetId(String newVal) {
+			String oldVal = this.datasetId;
+			this.datasetId = newVal;
+			firePropertyChange("datasetId", oldVal, newVal);
+		}
+		public String getStatus() {
+			return status;
+		}
+		public void setStatus(String newVal) {
+			String oldVal = this.status;
+			this.status = newVal;
+			firePropertyChange("status", oldVal, newVal);
+		}
+	}
+
+	private final class Exporter implements Runnable {
+		private final ExportInfo d_info;
+
+		private Exporter(ExportInfo info) {
+			d_info = info;
+		}
+
+		@Override
+		public void run() {
+			try {
+				d_info.setStatus("Checking connection...");
+				HttpClient client = HttpClients.createDefault();
+				URI meUri = new URIBuilder(d_info.getServer()).setPath("/whoami").build();
+				HttpGet get = new HttpGet(meUri);
+				get.setHeader("X-Auth-Application-Key", d_info.apiKey);
+				HttpResponse response = client.execute(get);
+				ObjectMapper mapper = new ObjectMapper();
+				Map parsed = mapper.readValue(response.getEntity().getContent(), Map.class);
+				Integer userId = (Integer) parsed.get("id"); 
+				
+				
+				d_info.setStatus("Generating XML...");
+				ByteArrayOutputStream os = new ByteArrayOutputStream();
+				d_mgr.saveXMLDomain(os);
+				String xml = new String(os.toByteArray(), StandardCharsets.UTF_8);
+				
+				d_info.setStatus("Converting to RDF...");
+				String rdf = AddisToRdfFactory.create().convert(xml, DATASET_URI_PREFIX + d_info.getDatasetId(), d_info.getName(), d_info.getTitle());
+				
+				d_info.setStatus("Uploading RDF...");
+				URI writeDatasetUri = new URIBuilder(d_info.getServer())
+						.setPath("/users/" + userId + "/datasets/" + d_info.getDatasetId())
+		        .setParameter("commitTitle", "Exported from ADDIS 1.x")
+		        .build();
+				System.err.println(writeDatasetUri);
+				HttpPost post = new HttpPost(writeDatasetUri);
+				post.setHeader("Content-Type", "text/trig");
+				post.setHeader("X-Auth-Application-Key", d_info.apiKey);
+				post.setEntity(new ByteArrayEntity(rdf.getBytes()));
+				HttpResponse execute = client.execute(post);
+				d_info.setStatus("Response status: " + execute.getStatusLine());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private DomainManager d_mgr;
+	
+	private static final String DATASET_URI_PREFIX = "http://trials.drugis.org/datasets/";
+
+	public Addis2ExportDialog(JFrame parent, DomainManager mgr) {
+		super(parent, "Export to ADDIS 2", true);
+		d_mgr = mgr;
+		final ExportInfo info = new ExportInfo();
+		Addis2ExportView view = new Addis2ExportView(new PresentationModel<ExportInfo>(info), new Exporter(info));
+		add(view.buildPanel());
+		pack();
+	}
+
+}
---
+++ application/src/main/java/org/drugis/addis/gui/builder/Addis2ExportView.java
@@ -0,0 +1,148 @@
+/*
+ * This file is part of ADDIS (Aggregate Data Drug Information System).
+ * ADDIS is distributed from http://drugis.org/.
+ * Copyright © 2009 Gert van Valkenhoef, Tommi Tervonen.
+ * Copyright © 2010 Gert van Valkenhoef, Tommi Tervonen, Tijs Zwinkels,
+ * Maarten Jacobs, Hanno Koeslag, Florin Schimbinschi, Ahmad Kamal, Daniel
+ * Reid.
+ * Copyright © 2011 Gert van Valkenhoef, Ahmad Kamal, Daniel Reid, Florin
+ * Schimbinschi.
+ * Copyright © 2012 Gert van Valkenhoef, Daniel Reid, Joël Kuiper, Wouter
+ * Reckman.
+ * Copyright © 2013 Gert van Valkenhoef, Joël Kuiper.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.drugis.addis.gui.builder;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.drugis.addis.gui.Addis2ExportDialog.ExportInfo;
+import org.drugis.addis.gui.util.NonEmptyValueModel;
+import org.drugis.common.gui.BuildViewWhenReadyComponent;
+import org.drugis.common.gui.ViewBuilder;
+import org.drugis.common.threading.SimpleSuspendableTask;
+import org.drugis.common.threading.ThreadHandler;
+import org.drugis.common.validation.BooleanAndModel;
+
+import com.jgoodies.binding.PresentationModel;
+import com.jgoodies.binding.adapter.BasicComponentFactory;
+import com.jgoodies.binding.adapter.Bindings;
+import com.jgoodies.binding.value.ValueModel;
+import com.jgoodies.forms.builder.PanelBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+public class Addis2ExportView implements ViewBuilder {
+	private ValueModel d_rdfReady;
+	private BooleanAndModel d_validator = new BooleanAndModel();
+	
+	private JTextField d_nameField;
+	private JTextField d_titleField;
+	private JTextField d_apiKeyField;
+	private JTextField d_serverField;
+	private JTextField d_datasetIdField;
+	private JButton d_importButton;
+	private JLabel d_statusLabel;
+
+	private JPanel d_panel;
+	private BuildViewWhenReadyComponent d_rdfReadyWaiter;
+	private PresentationModel<ExportInfo> d_pm;
+	private Runnable d_export;
+	
+	public Addis2ExportView(PresentationModel<ExportInfo> presentationModel, Runnable export) {
+		d_pm = presentationModel;
+		d_export = export;
+	}
+
+	public void initComponents() {
+		d_validator.add(new NonEmptyValueModel(d_pm.getModel("name")));
+		d_validator.add(new NonEmptyValueModel(d_pm.getModel("title")));
+		d_validator.add(new NonEmptyValueModel(d_pm.getModel("apiKey")));
+		d_validator.add(new NonEmptyValueModel(d_pm.getModel("server")));
+		d_validator.add(new NonEmptyValueModel(d_pm.getModel("datasetId")));
+//		d_validator.add(d_rdfReady);
+		
+		d_nameField = BasicComponentFactory.createTextField(d_pm.getModel("name"), false);
+		d_nameField.setColumns(15);
+		d_titleField = BasicComponentFactory.createTextField(d_pm.getModel("title"), false);
+		d_titleField.setColumns(30);
+		d_apiKeyField = BasicComponentFactory.createTextField(d_pm.getModel("apiKey"), false);
+		d_apiKeyField.setColumns(30);
+		d_serverField = BasicComponentFactory.createTextField(d_pm.getModel("server"), false);
+		d_serverField.setColumns(30);
+		d_datasetIdField = BasicComponentFactory.createTextField(d_pm.getModel("datasetId"), false);
+		d_datasetIdField.setColumns(30);
+		d_statusLabel = BasicComponentFactory.createLabel(d_pm.getModel("status"));
+		d_importButton = new JButton("Import");
+		Bindings.bind(d_importButton, "enabled", d_validator);
+		d_importButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ThreadHandler.getInstance().scheduleTask(new SimpleSuspendableTask(d_export, "Export to ADDIS 2"));
+			}
+		});
+	}
+
+	private ViewBuilder rdfReadyPanelBuilder() {
+		return new ViewBuilder() {
+			@Override
+			public JComponent buildPanel() {
+				return new JLabel("Dataset ready!");
+			}
+		};
+	}
+
+	public JComponent buildPanel() {
+		initComponents();
+		
+		FormLayout layout = new FormLayout(
+				"right:pref, 3dlu, pref, 3dlu, pref",
+				"p, 3dlu, p, 3dlu, p, 3dlu, p, 3dlu, p, 3dlu, p, 3dlu, p, 3dlu, p"
+				);	
+		
+		PanelBuilder builder = new PanelBuilder(layout);
+		builder.setDefaultDialogBorder();
+		
+		CellConstraints cc = new CellConstraints();
+		
+		builder.addSeparator("Export to ADDIS 2", cc.xyw(1, 1, 5));
+		builder.addLabel("Name:", cc.xy(1, 3));
+		builder.add(d_nameField, cc.xy(3, 3));
+		builder.addLabel("Title:", cc.xy(1, 5));
+		builder.add(d_titleField, cc.xyw(3, 5, 3));
+		builder.addLabel("API key:", cc.xy(1, 7));
+		builder.add(d_apiKeyField, cc.xyw(3, 7, 3));
+		builder.addLabel("ADDIS server:", cc.xy(1, 9));
+		builder.add(d_serverField, cc.xyw(3, 9, 3));
+		builder.addLabel("Dataset ID:", cc.xy(1, 11));
+		builder.add(d_datasetIdField, cc.xyw(3, 11, 3));
+
+		builder.add(d_statusLabel, cc.xyw(1, 13, 3));
+		builder.add(d_importButton, cc.xy(5, 13));
+		
+//		builder.add(d_rdfReadyWaiter, cc.xyw(1, 9, 5));
+		
+		d_panel = builder.getPanel();
+		return d_panel;	
+	}
+}
